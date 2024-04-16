1bea865053c8012f9d9be290da0145c18db78b6d
dkoleksandr <didenk.oleksandr@gmail.com>
2024-04-17 00:40:03 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,19 @@
 import axios from 'axios';
 
-export { fetchData, BASE_URL, params, page, per_page, updateSearchValue, updatePageValue };
+export {
+  fetchData,
+  BASE_URL,
+  params,
+  page,
+  per_page,
+  updateSearchValue,
+  updatePageValue,
+};
 import { page, per_page } from '../main';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '43338805-0211d3d1e83cb5c165622303b';
 let searchValue;
-
 
 let params = {
   key: API_KEY,
@@ -15,17 +22,17 @@ let params = {
   orientation: 'horizontal',
   safesearch: true,
   page,
-  per_page
+  per_page,
 };
 
 async function fetchData(url, options = {}) {
-  return await axios(url, { params })
+  return await axios(url, { params });
 }
 
-function updateSearchValue(search ) {
+function updateSearchValue(search) {
   params.q = search;
 }
 
-function updatePageValue(page){
+function updatePageValue(page) {
   params.page = page;
 }
